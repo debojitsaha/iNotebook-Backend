3bd77d6260ce694a0cc0fe783688f23bfa141861
debojit <saha05debojit@gmail.com>
2022-09-01 22:03:38 +0530

    iNotebook deployement

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -10,7 +10,7 @@ const Signup = (props) => {
 
     if(credentials.password===credentials.cpassword){
 
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    const response = await fetch("https://inotebookmern.herokuapp.com/api/auth/createuser", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'  
